3fe238f47adbd3d9e7209878b137e94a1d0acf2f
Cori Fowler <fowler.cori@gmail.com>
2016-02-28 22:39:45 -0500

    initial commit

---
+++ app/mock-heroes.js
@@ -0,0 +1,22 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": 1, "name": "Darryl" },
+                { "id": 2, "name": "Isaac" },
+                { "id": 3, "name": "Maren" },
+                { "id": 4, "name": "Justin" },
+                { "id": 5, "name": "Nick" },
+                { "id": 6, "name": "Abbey" },
+                { "id": 7, "name": "Cori" },
+                { "id": 8, "name": "Nate" },
+                { "id": 9, "name": "Amit" },
+            ]);
+        }
+    }
+});
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export var HEROES: Hero[] = [
+    { "id": 1, "name": "Darryl" },
+    { "id": 2, "name": "Isaac" },
+    { "id": 3, "name": "Maren" },
+    { "id": 4, "name": "Justin" },
+    { "id": 5, "name": "Nick" },
+    { "id": 6, "name": "Abbey" },
+    { "id": 7, "name": "Cori" },
+    { "id": 8, "name": "Nate" },
+    { "id": 9, "name": "Amit" },
+];
